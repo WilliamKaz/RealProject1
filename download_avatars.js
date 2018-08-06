129b1858ec03298e6b0ba1687903426d824ab292
WilliamKasurak <willkdesign@gmail.com>
2018-08-06 23:18:07 +0000

    almost there get user

--- download_avatars.js
+++ download_avatars.js
@@ -1,10 +1,13 @@
 var https = require('https');
-var request = require('request')
+var request = require('request');
+var secret = require('./secret.js');
 var Process = process.argv.slice(2)
 var repo = Process[1];
 var userName = Process[0];
 var gotRepo;
 var gotUser;
+var avUrl = [];
+var container = [];
 
 
 // function getRepoContributors(repoOwner, repoName, cb) {
@@ -18,12 +21,30 @@ var gotUser;
 
 // }
 function getRepoContributors(repoOwner, repoName, cb) {
-  var url = "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors";
-  request(url, function(err, res, body) {
+   var options = {
+    url: "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
+    headers: {
+      'User-Agent': 'request',
+      'Authorization': 'token ' + secret.GITHUB_TOKEN
+    }
+
+  };
+
+  request(options, function(err, res, body) {
     cb(err, body);
   });
 }
-getRepoContributors(userName, repo )
+
+getRepoContributors("jquery", "jquery", function(err, result) {
+  console.log("Errors:", err);
+  // console.log("Result:", result);
+  // console.log( JSON.parse(result));
+  for (var i = 0; i < result.length; i++) {
+     container.push(JSON.parse(result[i].avatar_url));
+  }
+  console.log( container);
+});
+// getRepoContributors(userName, repo )
 
 console.log('Welcome to the GitHub Avatar Downloader!');
 
