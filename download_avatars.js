23a63644d6bd6a16931555371d6aa5e9e28885eb
WilliamKasurak <willkdesign@gmail.com>
2018-08-07 00:17:15 +0000

    complete

--- download_avatars.js
+++ download_avatars.js
@@ -1,54 +1,45 @@
-var https = require('https');
+var fs =require('fs');
 var request = require('request');
 var secret = require('./secret.js');
-var Process = process.argv.slice(2)
+var Process = process.argv.slice(2);
 var repo = Process[1];
-var userName = Process[0];
+var owner= Process[0];
 var gotRepo;
-var gotUser;
-var avUrl = [];
 var container = [];
 
-
-// function getRepoContributors(repoOwner, repoName, cb) {
-//     console.log("Errors:", err);
-//     request.get('https://api.github.com/repos/jquery/jquery/contributors')
-//     .on('response', function(response){
-//       console.log(response.statusCode + " " +response.headers['content-type'] );
-//     })
-
-//     console.log("Result:", result);
-
-// }
 function getRepoContributors(repoOwner, repoName, cb) {
+  if (repoOwner === undefined || repoName === undefined ) {
+    console.log('YOU goofed, please pass the required arguments');
+  } else {
    var options = {
     url: "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
     headers: {
       'User-Agent': 'request',
       'Authorization': 'token ' + secret.GITHUB_TOKEN
     }
-
   };
 
   request(options, function(err, res, body) {
     cb(err, body);
   });
+  }
 }
 
-getRepoContributors("jquery", "jquery", function(err, result) {
-  console.log("Errors:", err);
-  // console.log("Result:", result);
-  // console.log( JSON.parse(result));
-  for (var i = 0; i < result.length; i++) {
-     container.push(JSON.parse(result[i].avatar_url));
-  }
-  console.log( container);
+
+getRepoContributors(owner, repo, function(err, result) {
+    console.log("Errors:", err);
+    gotRepo =  JSON.parse(result);
+    container = gotRepo.forEach(function(user){
+    downloadImageByURL(user.avatar_url, user.login) });
 });
-// getRepoContributors(userName, repo )
+
+
+function downloadImageByURL(url, filePath) {
+    request.get(url).pipe(fs.createWriteStream('./avatars/' + filePath + '.png'));
+
+  // fs.writeFile()
+}
+
+
 
 console.log('Welcome to the GitHub Avatar Downloader!');
-
-// getRepoContributors("jquery", "jquery", function(err, result) {
-
-// });
-
